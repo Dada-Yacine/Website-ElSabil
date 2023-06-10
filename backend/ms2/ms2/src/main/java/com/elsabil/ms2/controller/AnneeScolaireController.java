70f530f4c4cefe0f113939967eda5bc6394b1779
Kubx <hanikubx@gmail.com>
2023-06-10 21:54:52 +0100

    second commit ms2
    ajout de LastAnneeScolaire Function

--- backend/ms2/ms2/src/main/java/com/elsabil/ms2/controller/AnneeScolaireController.java
+++ backend/ms2/ms2/src/main/java/com/elsabil/ms2/controller/AnneeScolaireController.java
@@ -1,8 +1,11 @@
 package com.elsabil.ms2.controller;
 
 import com.elsabil.ms2.entities.AnneeScolaire;
+import com.elsabil.ms2.repositories.AnneeRepository;
+import com.elsabil.ms2.repositories.AnneeScolaireRepository;
 import com.elsabil.ms2.services.AnneeScolaireService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +15,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/annees-scolaires")
 public class AnneeScolaireController {
-    private AnneeScolaireService anneeScolaireService; // Injectez ou instanciez le anneeScolaireService approprié
+    private AnneeScolaireService anneeScolaireService;
+
+    @Autowired
+    private AnneeScolaireRepository anneeScolaireRepository;
 
     @Autowired
     public AnneeScolaireController(AnneeScolaireService anneeScolaireService) {
@@ -43,5 +49,13 @@ public class AnneeScolaireController {
     @PatchMapping("/{id}")
     public void updateAnneeScoalire(@PathVariable Long id, @RequestBody AnneeScolaire anneeScolaire) throws Exception {
         anneeScolaireService.updateAnneeScolaire(id,anneeScolaire);
+    }
+
+
+    @GetMapping("/last")
+    public AnneeScolaire getLastAnneeScolaire( ){
+        List<AnneeScolaire> anneeScolaireList = anneeScolaireRepository.findAll();
+        int anneeScolaireListSize = anneeScolaireList.size();
+        return anneeScolaireList.get(anneeScolaireListSize - 1);
     }
 }
